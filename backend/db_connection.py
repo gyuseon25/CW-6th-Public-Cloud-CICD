79b1a8eebfe7a9fdb651861f03c59273c70988a9
gyuseon25 <mn5592@naver.com>
2025-07-24 23:56:08 +0000

    update login function

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        host='lab-edu-rds-aurora.cluster-ro-cxec8ssuqluu.ap-northeast-2.rds.amazonaws.com',
+        host='lab-edu-rds-aurora.cluster-cxec8ssuqluu.ap-northeast-2.rds.amazonaws.com',
         database='trip_advisor',
         user='user',
         password='qwer1234'
